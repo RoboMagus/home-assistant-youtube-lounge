8e649984bd4bf6132b0020fd84d28e3c311327ec
RoboMagus <68224306+RoboMagus@users.noreply.github.com>
2026-01-26 22:07:28 +0000

    Fix reauth config flow

--- custom_components/yt_lounge/config_flow.py
+++ custom_components/yt_lounge/config_flow.py
@@ -40,6 +40,38 @@ def _test_api_key(key: str) -> bool:
     LOGGER.debug(f"YT API Response: {details}")
     return True
 
+async def _test_tv_key(pairing_code: str) -> Dict[str, Any]:
+    async with YtLoungeApi("HA ConfigFlow", None, logging.getLogger(f"{__package__}.pyytlounge")) as client:
+        LOGGER.debug(f"Pairing with code {pairing_code}...")
+        paired = await client.pair(pairing_code)
+        LOGGER.debug(paired and "success" or "failed")
+        if not paired:
+            raise InvalidAuth
+
+        is_available = await client.is_available()
+        LOGGER.debug(f"Screen availability: {is_available}")
+
+        LOGGER.debug("Connecting...")
+        connected = await client.connect()
+        LOGGER.debug(connected and "success" or "failed")
+        if not connected:
+            raise CannotConnect
+
+        auth_state = client.auth.serialize()
+        screen_id = client.auth.screen_id
+        screen_name = client.screen_name
+        device_name = client.screen_device_name
+        LOGGER.debug(f"AuthState: {auth_state}")
+        LOGGER.debug(f"ScreenName: {screen_id}")
+        LOGGER.debug(f"ScreenName: {screen_name}")
+        LOGGER.debug(f"DeviceName: {device_name}")
+
+        return {
+            "auth_state": auth_state,
+            "screen_id": screen_id,
+            "screen_name": screen_name,
+            "device_name": device_name
+        }
 
 class YTLoungeConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Jellyfin."""
@@ -64,32 +96,7 @@ class YTLoungeConfigFlow(ConfigFlow, domain=DOMAIN):
                     if not api_key_valid:
                         raise InvalidApiKey
 
-                async with YtLoungeApi("HA ConfigFlow", None, logging.getLogger(f"{__package__}.pyytlounge")) as client:
-                    pairing_code = user_input[CONF_TV_CODE]
-                    LOGGER.debug(f"Pairing with code {pairing_code}...")
-                    paired = await client.pair(pairing_code)
-                    LOGGER.debug(paired and "success" or "failed")
-                    if not paired:
-                        raise CannotConnect
-
-                    is_available = await client.is_available()
-                    LOGGER.debug(f"Screen availability: {is_available}")
-
-                    LOGGER.debug("Connecting...")
-                    connected = await client.connect()
-                    LOGGER.debug(connected and "success" or "failed")
-                    if not connected:
-                        raise CannotConnect
-
-                    auth_state = client.auth.serialize()
-                    screen_id = client.screen_id
-                    screen_name = client.screen_name
-                    device_name = client.screen_device_name
-                    LOGGER.debug(f"AuthState: {auth_state}")
-                    LOGGER.debug(f"ScreenName: {screen_id}")
-                    LOGGER.debug(f"ScreenName: {screen_name}")
-                    LOGGER.debug(f"DeviceName: {device_name}")
-
+                tv_pair_results = await _test_tv_key(user_input[CONF_TV_CODE])
 
             except CannotConnect:
                 errors["base"] = "cannot_connect"
@@ -102,17 +109,17 @@ class YTLoungeConfigFlow(ConfigFlow, domain=DOMAIN):
                 LOGGER.exception("Unexpected exception")
             else:
 
-                await self.async_set_unique_id(auth_state['screenId'])
+                await self.async_set_unique_id(tv_pair_results['auth_state']['screenId'])
                 self._abort_if_unique_id_configured()
 
                 return self.async_create_entry(
-                    title=screen_name,
+                    title=tv_pair_results['screen_name'],
                     data={
-                        CONF_AUTH_STATE: auth_state,
                         CONF_API_KEY: user_input[CONF_API_KEY],
-                        CONF_DEVICE_NAME: device_name,
-                        CONF_SCREEN_NAME: screen_name,
-                        CONF_SCREEN_ID: screen_id,
+                        CONF_AUTH_STATE: tv_pair_results['auth_state'],
+                        CONF_DEVICE_NAME: tv_pair_results['device_name'],
+                        CONF_SCREEN_NAME: tv_pair_results['screen_name'],
+                        CONF_SCREEN_ID: tv_pair_results['screen_id'],
                     },
                 )
 
@@ -136,29 +143,36 @@ class YTLoungeConfigFlow(ConfigFlow, domain=DOMAIN):
         """Dialog that informs the user that reauth is required."""
         errors: dict[str, str] = {}
 
-        if user_input is not None:
-            reauth_entry = self._get_reauth_entry()
-            new_input = reauth_entry.data | user_input
+        if not user_input:
+            return self.async_show_form(
+                step_id="reauth_confirm", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            )
 
-            if self.client_device_id is None:
-                self.client_device_id = _generate_client_device_id()
+        try:
+            if CONF_API_KEY in user_input:
+                api_key_valid = await self.hass.async_add_executor_job(_test_api_key, user_input[CONF_API_KEY])
+                if not api_key_valid:
+                    raise InvalidApiKey
+            tv_pair_results = await _test_tv_key(user_input[CONF_TV_CODE])
+        except CannotConnect:
+            errors["base"] = "cannot_connect"
+        except InvalidAuth:
+            errors["base"] = "invalid_auth"
+        except InvalidApiKey:
+            errors["base"] = "invalid_api_key"
+        except Exception:
+            errors["base"] = "unknown"
+            LOGGER.exception("Unexpected exception")
+        else:
+            data={
+                CONF_AUTH_STATE: tv_pair_results['auth_state'],
+            }
+            if CONF_API_KEY in user_input:
+                data[CONF_API_KEY] = user_input[CONF_API_KEY]
 
-            client = create_client(device_id=self.client_device_id)
-            try:
-                await validate_input(self.hass, new_input, client)
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
-            except InvalidAuth:
-                errors["base"] = "invalid_auth"
-            except Exception:
-                errors["base"] = "unknown"
-                LOGGER.exception("Unexpected exception")
-            else:
-                return self.async_update_reload_and_abort(reauth_entry, data=new_input)
-
-        return self.async_show_form(
-            step_id="reauth_confirm", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
-        )
+            return self.async_update_reload_and_abort(
+                self._get_reauth_entry(), data_updates=data
+            )
 
 class CannotConnect(exceptions.HomeAssistantError):
     """Error to indicate the server is unreachable."""
