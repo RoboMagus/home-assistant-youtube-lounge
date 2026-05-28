d97de76964d6976807467133cffb7e8d8d82aee8
RoboMagus <68224306+RoboMagus@users.noreply.github.com>
2026-05-28 21:08:13 +0000

    - Bump devcontainer base image to 2026.5 - Install HACS during Setup - Fix custom component symlink

--- .devcontainer/scripts/setup.sh
+++ .devcontainer/scripts/setup.sh
@@ -9,5 +9,13 @@ hass -c ha-config --script ensure_config
 
 # Symlink custom integration into HA Config directory
 mkdir -p ha-config/custom_components
-ln -sf custom_components/yt_lounge ha-config/custom_components/yt_lounge
+ln -sf ../../custom_components/yt_lounge ha-config/custom_components/yt_lounge
 
+# Install HACS
+if [ ! -d "ha-config/custom_components/hacs" ]; then
+    mkdir -p ha-config/custom_components/hacs
+    echo "Downloading HACS"
+    wget "https://github.com/hacs/integration/releases/latest/download/hacs.zip" -O hacs.zip
+    unzip hacs.zip -d ha-config/custom_components/hacs >/dev/null 2>&1
+    rm hacs.zip
+fi
